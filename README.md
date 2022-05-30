08a9d4ae13edf1ed1b67b8904b7fed7b2416cec9
irvyn <irvyncornejo@gmail.com>
2022-05-29 20:33:27 -0500

    Se añaden las imagenes del resultado

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# Nextion 2.4"
+# Display Nextion 2.4" 
+## Uso de display nextion para crear HMI o interfaz gráfica para un microcontrolador.
+
+## DIY-MACRO-KEYPAD
+### Lista de materiales:
+  - Raspberrypi pico
+  - Led RGB
+  - Regulador de voltaje
+  - [Case impreso 3D](https://github.com/irvyncornejo/nextion-display/tree/main/pico-diy-stream-deck/src/case-3D)
+
+### [Código](https://github.com/irvyncornejo/nextion-display/tree/main/pico-diy-stream-deck/programming-processor)
+
+![img_1](https://github.com/irvyncornejo/nextion-display/tree/main/pico-diy-stream-deck/result/1.jpeg)
+
+![img_1](https://github.com/irvyncornejo/nextion-display/tree/main/pico-diy-stream-deck/result/2.jpeg)
+
+![img_1](https://github.com/irvyncornejo/nextion-display/tree/main/pico-diy-stream-deck/result/3.jpeg)
+
+  
+## ARDUINO-DISPLAY-IOT
+### Lista de materiales:
+  - Arduino Nano
+  - Shield Chimalli
+  - ESP8266
+
+### [Código](https://github.com/irvyncornejo/nextion-display/tree/main/arduino-display-for-IoT/programming-processor)
--- pico-diy-stream-deck/programming-processor/code.py
+++ pico-diy-stream-deck/programming-processor/code.py
@@ -4,8 +4,8 @@ Board: Raspberry Pi Pico with rp2040
 Display: NX3224T024 yellow = Rx | Blue = Tx
 """
 from time import sleep
-
 import board
+import pwmio
 import digitalio
 import busio
 import rotaryio
@@ -14,27 +14,61 @@ from adafruit_hid.keycode import Keycode
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.consumer_control import ConsumerControl
 from adafruit_hid.consumer_control_code import ConsumerControlCode
-from rgb import RGB
 
 #Objects
 keyboard=Keyboard(usb_hid.devices)
 consumer_control=ConsumerControl(usb_hid.devices)
 serial=busio.UART(board.GP4, board.GP5, baudrate=9600)#UART1 tx - rx
-rgb = RGB()
+
+max_values_led = 65535
+steps = 20
+step = max_values_led // steps 
 
 colors = {
-    '1': 'red',
-    '2': 'green',
-    '3': 'blue',
-    '4': {'red':32000,'blue':40000,'green':65535}
+    '5': {
+        'color': 'red',
+        'value': 65535
+    },
+    '6': {
+        'color':'green',
+        'value': 65535
+    },
+    '7': {
+        'color':'blue',
+        'value': 65535
+    }
 }
+
+rgb_led = {
+    'blue': pwmio.PWMOut(board.GP21, frequency=5000, duty_cycle=0),
+    'green': pwmio.PWMOut(board.GP20, frequency=5000, duty_cycle=0),
+    'red': pwmio.PWMOut(board.GP19, frequency=5000, duty_cycle=0),
+}
+
+class RGB:
+    
+    def __init__(self):
+        self.off_value = 65535
+    
+    @staticmethod
+    def select_color(color):
+        for led in color.keys():
+            rgb_led[led].duty_cycle = color[led]
+            
+    def change_color(self, color:str, step:int) -> None:
+        led = rgb_led[color]
+        led.duty_cycle = int(step)
+            
+    def off_rgb(self) -> None:
+        rgb_led['blue'].duty_cycle = self.off_value
+        rgb_led['green'].duty_cycle = self.off_value
+        rgb_led['red'].duty_cycle = self.off_value
+    
 
 def get_operating_system(page: int ,component: int) -> str:
     pass
 
-def define_action(page: int ,component: int) -> None:
-    """
-    """
+def define_action(page: int, component: int) -> None:
     if page==0 and component==1:
         consumer_control.send(ConsumerControlCode.VOLUME_INCREMENT)
     if page==0 and component==2:
@@ -43,26 +77,37 @@ def define_action(page: int ,component: int) -> None:
         keyboard.send(Keycode.CONTROL, Keycode.ALT, Keycode.G)
     if page==0 and component==4:
         keyboard.send(Keycode.CONTROL, Keycode.ALT, Keycode.Y)
-    if page==0 and component==5:
-        keyboard.send(Keycode.WINDOWS, Keycode.E)
     if page==0 and component==6:
-        keyboard.send(Keycode.CONTROL, Keycode.ALT, Keycode.P)
-    if page==1 and component==3:
-        rgb.off_rgb()
-        rgb.blink_rgb()
+        keyboard.send(Keycode.CONTROL, Keycode.ALT, Keycode.S)
+    if page==1 and component==2:
+        keyboard.send(Keycode.WINDOWS, Keycode.E)
+    if page==1 and component==4:
+        keyboard.send(Keycode.WINDOWS, Keycode.I)
+    if page==0 and component==6:
+        keyboard.send(Keycode.CONTROL, Keycode.ALT, Keycode.A)
     if page==2:
-        rgb.off_rgb()
-        color = colors[str(component)]
-        rgb.turn_on(color)
-        
+        update_value = lambda component, value : colors[component].update({'value': value})
+        component = str(component)
+        color = colors[component]['color']
+        value_color = colors[component]['value'] - step
+        if value_color > 0:
+            update_value(component, value_color)
+            rgb.change_color(color, value_color)
+        else:
+            update_value(component, max_values_led)
+            rgb.change_color(color, max_values_led)
+    
 
 if __name__ == '__main__':
+    rgb = RGB()
     rgb.off_rgb()
+    
     while True:
         try:
             data=serial.read(7)#number of bytes b'e\x00\x02\x01\xff\xff\xff'
             if len(data) == 7:
+                print(data)
                 define_action(data[1], data[2])
-        except:
-            #Exception for data=None
+        except Exception as e:
+            #print(f'error --> {e}')
             pass
