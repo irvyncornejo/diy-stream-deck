71188f004bd5e1fe108396b6c61a65668938d072
irvyn <irvyncornejo@gmail.com>
2022-12-31 22:01:02 -0600

    Add hardware control

--- weather-station/code/Applications.py
+++ weather-station/code/Applications.py
@@ -1,5 +1,5 @@
 from HTTP import HTTP
-
+from umachine import Pin, PWM
 
 class Applications:
     def __init__(self):
@@ -10,4 +10,29 @@ class Applications:
             print(self._http.get('http://date.jsontest.com'))
         else:
             pass
+
+class Actuators:
+    def __init__(
+        self,
+        pin: int,
+        mode:str
+    ) -> None:
+        self._pin = (
+            Pin("LED", Pin.OUT) if mode == 'output' else Pin(pin, Pin.IN)
+        )
+    
+    def changeState(self, state:bool) -> None:
+        if state:
+            self._pin.on()
+        self._pin.off()
+    
+    def changeValue(self, value:int) -> None:
+        pass
+
+class NextionDisplay:
+    pass
+
+class Servo:
+    pass
+
         
